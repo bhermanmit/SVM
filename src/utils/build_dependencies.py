d44e14c109f72db20d8370049024ae03349243e6
Bryan Herman <bherman@mit.edu>
2013-06-08 14:37:25 -0400

    added basic files

---
+++ src/utils/build_dependencies.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import glob
+import re
+
+dependencies = {}
+
+for src in glob.iglob('*.F90'):
+    module = src.strip('.F90')
+    deps = set()
+    d = re.findall(r'\n\s*use\s+(\w+)',
+                   open(src,'r').read())
+    for name in d:
+        if name in ['mpi','hdf5','h5lt']:
+            continue
+        if name.startswith('xml_data_'):
+            name = name.replace('xml_data_', 'templates/')
+        deps.add(name)
+    if deps:
+        dependencies[module] = sorted(list(deps))
+
+
+keys = dependencies.keys()
+keys.sort()
+for module in keys:
+    for dep in dependencies[module]:
+        print("{0}.o: {1}.o".format(module, dep))
+    print('')
